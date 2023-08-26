643e79fef769d6cf6d1153847bf28b66ce76ca68
edwin <zj726381216@163.com>
2023-08-26 10:58:39 +0800

    update: vpet改用sdk风格csproj，VPet-Simulator.Core支持net6.0

--- VPet-Simulator.Windows/Properties/AssemblyInfo.cs
+++ VPet-Simulator.Windows/Properties/AssemblyInfo.cs
@@ -1,23 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
-
-// 有关程序集的一般信息由以下
-// 控制。更改这些特性值可修改
-// 与程序集关联的信息。
-[assembly: AssemblyTitle("虚拟桌宠模拟器")]
-[assembly: AssemblyDescription("VPET的独立窗口实现")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LB Game")]
-[assembly: AssemblyProduct("VPet-Simulator.Windows")]
-[assembly: AssemblyCopyright("Copyright © exLB.org 2022")]
-[assembly: AssemblyTrademark("exLB.org")]
-[assembly: AssemblyCulture("")]
-
-// 将 ComVisible 设置为 false 会使此程序集中的类型
-//对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型
-//请将此类型的 ComVisible 特性设置为 true。
-[assembly: ComVisible(false)]
+﻿using System.Windows;
 
 //若要开始生成可本地化的应用程序，请设置
 //.csproj 文件中的 <UICulture>CultureYouAreCodingWith</UICulture>
@@ -37,17 +18,3 @@ using System.Windows;
                                               //(未在页面中找到资源时使用，
                                               //、应用程序或任何主题专用资源字典中找到时使用)
 )]
-
-
-// 程序集的版本信息由下列四个值组成: 
-//
-//      主版本
-//      次版本
-//      生成号
-//      修订号
-//
-//可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
-//通过使用 "*"，如下所示:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
