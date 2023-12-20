ffedc406a2d4962a38d55a8f1753fd97372e188a
Hakoyu <Hakoyu@outlook.com>
2023-12-20 20:51:58 +0800

    VPet.Solution 结构初始化

---
+++ VPet.Solution/Views/InteractiveSettingsPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VPet.Solution.Views;
+/// <summary>
+/// InteractiveSettingsPage.xaml 的交互逻辑
+/// </summary>
+public partial class InteractiveSettingsPage : Page
+{
+    public InteractiveSettingsPage()
+    {
+        InitializeComponent();
+    }
+}
