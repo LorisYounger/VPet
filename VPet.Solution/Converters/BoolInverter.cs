944cf2c34b9a4a2a9624a2cee62cff9957442ac6
Hakoyu <Hakoyu@outlook.com>
2023-12-25 00:31:22 +0800

    初始化 `InteractiveSettingsPage`

---
+++ VPet.Solution/Converters/BoolInverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace VPet.House.Converters;
+
+public class BoolInverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not bool b)
+            return false;
+        return !b;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
