57be820e16b3a690b87958bafd530c7e6c6dc473
ZouJin <zoujin.dev@exlb.org>
2024-03-11 15:22:23 +0800

    好友相关功能 TODO

---
+++ VPet-Simulator.Windows/MWFriends.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Panuon.WPF.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VPet_Simulator.Windows;
+/// <summary>
+/// MWFriends.xaml 的交互逻辑
+/// </summary>
+public partial class MWFriends : WindowX
+{
+
+    public MWFriends()
+    {
+        InitializeComponent();
+    }
+}
