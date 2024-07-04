ee92d365cea5c1339976e988921f062290362cae
ZouJin <zoujin.dev@exlb.org>
2024-07-04 14:47:09 +0800

    抄写新增转笔动画

--- VPet-Simulator.Windows/MainWindow_Property.cs
+++ VPet-Simulator.Windows/MainWindow_Property.cs
@@ -20,7 +20,7 @@ public partial class MainWindow
     /// <summary>
     /// 版本号
     /// </summary>
-    public int version { get; } = 11021;
+    public int version { get; } = 11022;
     /// <summary>
     /// 版本号
     /// </summary>
