20e81171395989f9699340921dc311ba8c868d16
ZouJin <zoujin.dev@exlb.org>
2024-12-24 21:26:39 +0800

    圣诞节更新

--- VPet-Simulator.Windows/MainWindow_Property.cs
+++ VPet-Simulator.Windows/MainWindow_Property.cs
@@ -20,7 +20,7 @@ public partial class MainWindow
     /// <summary>
     /// 版本号
     /// </summary>
-    public int version { get; } = 11044;
+    public int version { get; } = 11045;
     /// <summary>
     /// 版本号
     /// </summary>
