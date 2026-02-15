48a03e1b95c4307eb67dcc620670b09a9f3b6dc7
ZouJin <zoujin.dev@exlb.org>
2026-02-15 20:53:18 +0800

    新年限时动画

--- VPet-Simulator.Windows/mklink.bat
+++ VPet-Simulator.Windows/mklink.bat
@@ -1,6 +1,3 @@
-%1 mshta vbscript:createobject("shell.application").shellexecute("%~s0","::","","runas",1)(window.close)&exit
-cd /d %~dp0
-
 mklink /d "%~dp0\bin\x64\Debug\net8.0-windows\mod" "%~dp0\mod"
 
 echo The following is the automatic link generation for other related MODs. If an error is prompted, it is a normal phenomenon and can be ignored
@@ -15,4 +12,5 @@ mklink /d "%~dp0\mod\1111_ChatGPTPlus" "%~dp0\..\..\VPet.Plugin.ChatGPTPlus\VPet
 mklink /d "%~dp0\mod\1101_EdgeTTS" "%~dp0\..\..\VPet.Plugin.Demo\VPet.Plugin.EdgeTTS\1101_EdgeTTS"
 mklink /d "%~dp0\mod\1110_ChatGPT" "%~dp0\..\..\VPet.Plugin.Demo\VPet.Plugin.ChatGPT\1110_ChatGPT" 
 mklink /d "%~dp0\mod\1111_MutiPlayerStream" "%~dp0\..\..\VPet.Plugin.Demo\VPet.MutiPlayer.Stream\1111_MutiPlayerStream" 
+mklink /d "%~dp0\mod\1123_MutiRedEnvelope" "%~dp0\..\..\VPet.Plugin.Demo\VPet.Plugin.MutiRedEnvelope\1123_MutiRedEnvelope" 
 pause
